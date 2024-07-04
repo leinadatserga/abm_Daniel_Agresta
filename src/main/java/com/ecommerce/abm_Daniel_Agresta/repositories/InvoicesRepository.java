ee7d400dd3f30fa5aacb0a919eaa7426ebbb863d
Daniel Agresta <leinadatserga@gmail.com>
2024-07-04 12:43:31 -0300

    Repositories creation

--- src/main/java/com/ecommerce/abm_Daniel_Agresta/repositories/InvoicesRepository.java
+++ src/main/java/com/ecommerce/abm_Daniel_Agresta/repositories/InvoicesRepository.java
@@ -1,9 +1,9 @@
 package com.ecommerce.abm_Daniel_Agresta.repositories;
 
-import com.ecommerce.abm_Daniel_Agresta.entities.Client;
+import com.ecommerce.abm_Daniel_Agresta.entities.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientsRepository extends JpaRepository<Client, Long> {
+public interface InvoicesRepository extends JpaRepository<Invoice, Long> {
 }
